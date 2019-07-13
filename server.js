202f59009c6887f4819262270221644de58be96d
esdalee <es.da.lee@gmail.com>
2019-07-13 11:18:49 -0400

    adding waitlist logic

--- server.js
+++ server.js
@@ -41,6 +41,9 @@ var reservations = [
   }
 ];
 
+// Waitlist
+var waitlist = [];
+
 // Routes
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
@@ -62,7 +65,6 @@ app.get("/api/reservations", function(req, res) {
   return res.json(reservations);
 });
 
-
 // Create New Characters - takes in JSON input
 app.post("/api/reservations", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
@@ -70,42 +72,21 @@ app.post("/api/reservations", function(req, res) {
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   newreservation.routeName = newreservation.name.replace(/\s+/g, "").toLowerCase();
-
   console.log(newreservation);
 
-  characters.push(newreservation);
+  // If there are more than 5 reservations, put into waitlist
+  if (reservations.length === 5) {
+    console.log("Sorry you're on the waitlist!");
+    waitlist.push(newreservation);
+    res.json(newreservation);
+  }
 
+  // If there are less than 5 reservations, insert into reservations
+  else {
+  reservations.push(newreservation);
   res.json(newreservation);
+  }
 });
-
-// Check for Unique IDs
-app.get("/api/reservations/:ID", function(req, res) {
-    var inputID = req.params.ID;
-    console.log(inputID);
-  
-    for (var i = 0; i < characters.length; i++) {
-      if (inputID === characters[i].uniqueID) {
-        console.log ("Existing ID, use another one!");
-        return false;
-      }
-    }
-    return res.json(true);
-});
-
-// Check for Waitlist
-app.get("/api/reservations/:ID", function(req, res) {
-    var inputID = req.params.ID;
-    console.log(inputID);
-  
-    for (var i = 0; i < characters.length; i++) {
-      if (inputID === characters[i].uniqueID) {
-        console.log ("Existing ID, use another one!");
-        return false;
-      }
-    }
-    return res.json(true);
-});
-
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
